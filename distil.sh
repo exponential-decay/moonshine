6def9ba34e65c941cbca3f5c854200c61e950ba8
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2018-08-18 18:24:35 -0500

    Fix distil commands

--- distil.sh
+++ distil.sh
@@ -4,13 +4,25 @@ set -eux
 MOON="moonshine"
 DIR="release"
 mkdir "$DIR"
-env GOOS=windows GOARCH=386 go build
+export GOOS=windows
+export GOARCH=386
+go build
 mv "$MOON".exe "${DIR}/${MOON}"-win386.exe
-env GOOS=windows GOARCH=amd64 go build
+export GOOS=windows
+export GOARCH=amd64
+go build
 mv "$MOON".exe "${DIR}/${MOON}"-win64.exe
-env GOOS=linux GOARCH=amd64 go build
+export GOOS=linux
+export GOARCH=amd64
+go build
 mv "$MOON" "${DIR}/${MOON}"-linux64
-env GOOS=darwin GOARCH=386 go build
+export GOOS=darwin
+export GOARCH=386
+go build
 mv "$MOON" "${DIR}/${MOON}"-darwin386
-env GOOS=darwin GOARCH=amd64 go build
+export GOOS=darwin
+export GOARCH=amd64
+go build
 mv "$MOON" "${DIR}/${MOON}"-darwinAmd64
+export GOOS=
+export GOARCH=
