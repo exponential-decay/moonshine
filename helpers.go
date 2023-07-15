2b564cf61874d53e11d29ed7d7bee8026cea3f3c
Ross Spencer <all.along.the.watchtower2001@gmail.com>
2023-07-15 15:18:29 +0200

    Remove Seed before Rand (now deprecated)

--- helpers.go
+++ helpers.go
@@ -2,12 +2,10 @@ package main
 
 import (
 	"math/rand"
-	"time"
 )
 
 // getRandom returns a positive random number for a given range.
 func getRandom(rangeInt int) int {
-	rand.Seed(time.Now().UTC().UnixNano())
 	randomNumber := rand.Intn(rangeInt)
 	return randomNumber
 }
